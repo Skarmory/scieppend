6d0f30135afd1acf361e4cde4fb3e91dcee5cb8e
Song Taezhou <song.taezhou@gmail.com>
2023-02-16 14:37:16 +0000

    Created StackArray
    The stack array is an entirely macro based "smart" array. The idea is to
    replicate the functionality of a dynamic array, such as  c++ vector, but
    without (necessarily) allocating the data memory on the heap.
    
    One benefit of this is that we can avoid a poiner indirection because
    the memory is part of the size of the anonymous struct. A downside is
    that is needs to be statically sized, but that's just the way it is.

---
+++ include/scieppend/core/stack_array.h
@@ -0,0 +1,54 @@
+#ifndef SCIEPPEND_CORE_STACK_ARRAY_H
+#define SCIEPPEND_CORE_STACK_ARRAY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Array with static sized contiguous memory directly inside it.
+ * Basically a normal array, except with helpful "functions"
+ */
+
+#define StackArray(typename, capacity)\
+    struct\
+    {\
+        typename data[(capacity)];\
+        int count;\
+    }
+
+#define stackarray_new(typename, capacity)\
+    malloc(sizeof(typename) * (capacity) + sizeof(int))
+
+#define stackarray_init(array)\
+    do\
+    { (array)->count = 0;\
+    } while(0)
+
+#define stackarray_add(array, item)\
+    do\
+    {\
+        if((array)->count == (sizeof (array)->data / sizeof (array)->data[0]) )\
+        {\
+            fprintf(stderr, "Stack array out of memory");\
+            abort();\
+        }\
+        memcpy(&((array)->data[(array)->count]), (item), sizeof (array)->data[0]);\
+        ++((array)->count);\
+    } while(0)
+
+#define stackarray_remove(array, index)\
+    do\
+    {\
+        memcpy(&((array)->data[(index)]), &((array)->data[(array)->count-1]), sizeof (array)->data[0]);\
+        memset(&((array)->data[(array)->count-1]), '\0', sizeof (array)->data[0]);\
+    } while(0)
+
+#define stackarray_get(array, index)\
+    (&((array)->data[index]))
+
+#define stackarray_set(array, index, item)\
+    do\
+    {\
+        (array)->data[(index)] = *item;\
+    } while(0)
+
+#endif
