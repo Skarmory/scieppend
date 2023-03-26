3d487898671f66421e4382f6f2e0cc4f0a03a116
Song Taezhou <song.taezhou@gmail.com>
2023-03-26 14:34:43 +0100

    Updated Cache tests following changes

--- include/scieppend/test/core/cache.h
+++ include/scieppend/test/core/cache.h
@@ -1,9 +1,7 @@
 #ifndef SCIEPPEND_TEST_CORE_CACHE_H
 #define SCIEPPEND_TEST_CORE_CACHE_H
 
-#include <stdbool.h>
-
-bool test_cache_iterator(void);
+void test_cache_iterator(void);
 void test_cache_run_all(void);
 
 #endif
--- src/test/core/cache.c
+++ src/test/core/cache.c
@@ -6,115 +6,104 @@
 #include <stdarg.h>
 #include <stddef.h>
 
+#define TEST_ELEMENTS_MAX 32
+
 struct TestItem
 {
     int   i;
     float f;
 };
 
-static bool _test_cache_iterator__cache_no_gaps(void)
+struct CacheTestState
 {
-    bool success = true;
-    struct Cache* cache = cache_new(sizeof(struct TestItem), 32, NULL, NULL);
+    struct Cache* cache;
+    int           handles[TEST_ELEMENTS_MAX];
+};
 
-    for(int i = 0; i < 32; ++i)
+static void _setup_cache([[maybe_unused]] void* userstate)
+{
+    struct CacheTestState* state = userstate;
+    state->cache = cache_new(sizeof(struct TestItem), 32, NULL, NULL);
+
+    for(int i = 0; i < TEST_ELEMENTS_MAX; ++i)
     {
         struct TestItem item;
         item.i = 32 - i;
         item.f = (float)i * 7.0f;
-        cache_add(cache, &item);
+        state->handles[i] = cache_add(state->cache, &item);
     }
-
-    int i = 0;
-    for(struct CacheIt it = cache_begin(cache); !cache_it_eq(it, cache_end(cache)); it = cache_it_next(it))
-    {
-        struct TestItem* item = cache_it_get(it);
-        success &= test_assert_equal_int(32 - i, item->i);
-        success &= test_assert_equal_float((float)i * 7.0f, item->f);
-        ++i;
-    }
-
-    cache_free(cache);
-
-    return success;
 }
 
-bool _test_cache_iterator__cache_with_gaps(void)
+static void _teardown_cache([[maybe_unused]] void* userstate)
 {
-    bool success = true;
-    struct Cache* cache = cache_new(sizeof(struct TestItem), 8, NULL, NULL);
+    struct CacheTestState* state = userstate;
+    cache_free(state->cache);
+}
 
-    int handles[8];
+static void _test_cache_iterator__cache_no_gaps([[maybe_unused]] void* userstate)
+{
+    struct CacheTestState* state = userstate;
 
-    for(int i = 0; i < 8; ++i)
+    int i = 0;
+    for(struct CacheIt it = cache_begin(state->cache); !cache_it_eq(it, cache_end(state->cache)); it = cache_it_next(it))
     {
-        struct TestItem item;
-        item.i = 8 - i;
-        item.f = (float)i * 7.0f;
-        handles[i] = cache_add(cache, &item);
+        struct TestItem* item = cache_it_get(it);
+        test_assert_equal_int("elem int value", 32 - i, item->i);
+        test_assert_equal_float("elem float value", (float)i * 7.0f, item->f);
+        ++i;
     }
+}
 
-    for(int i = 0; i < 8; i += 2)
+static void _test_cache_iterator__cache_with_gaps([[maybe_unused]] void* userstate)
+{
+    struct CacheTestState* state = userstate;
+
+    for(int i = 0; i < TEST_ELEMENTS_MAX; i += 2)
     {
-        cache_remove(cache, handles[i]);
+        cache_remove(state->cache, state->handles[i]);
     }
 
     int i = 1;
-    for(struct CacheIt it = cache_begin(cache); !cache_it_eq(it, cache_end(cache)); it = cache_it_next(it))
+    for(struct CacheIt it = cache_begin(state->cache); !cache_it_eq(it, cache_end(state->cache)); it = cache_it_next(it))
     {
         struct TestItem* item = cache_it_get(it);
-        success &= test_assert_equal_int(8 - i, item->i);
-        success &= test_assert_equal_float((float)i * 7.0f, item->f);
+        test_assert_equal_int("elem int value", TEST_ELEMENTS_MAX - i, item->i);
+        test_assert_equal_float("elem float value", (float)i * 7.0f, item->f);
         i += 2;
     }
-
-    cache_free(cache);
-    return success;
 }
 
-bool _test_cache_iterator__no_valid_items__items_removed(void)
+static void _test_cache_iterator__no_valid_items__items_removed([[maybe_unused]] void* userstate)
 {
-    bool success = true;
-    struct Cache* cache = cache_new(sizeof(struct TestItem), 8, NULL, NULL);
-    int handles[8];
+    struct CacheTestState* state = userstate;
 
-    for(int i = 0; i < 8; ++i)
+    for(int i = 0; i < TEST_ELEMENTS_MAX; ++i)
     {
-        struct TestItem item;
-        item.i = 8 - i;
-        item.f = (float)i * 7.0f;
-        handles[i] = cache_add(cache, &item);
+        cache_remove(state->cache, state->handles[i]);
     }
 
-    for(int i = 0; i < 8; ++i)
-    {
-        cache_remove(cache, handles[i]);
-    }
-
-    success &= test_assert_equal_int(0, cache_size(cache));
+    test_assert_equal_int("cache size", 0, cache_size(state->cache));
 
     int i = 0;
-    for(struct CacheIt it = cache_begin(cache); !cache_it_eq(it, cache_end(cache)); it = cache_it_next(it))
+    for(struct CacheIt it = cache_begin(state->cache); !cache_it_eq(it, cache_end(state->cache)); it = cache_it_next(it))
     {
         ++i;
     }
 
-    success &= test_assert_equal_int(0, i);
-
-    cache_free(cache);
-    return success;
+    test_assert_equal_int("iterator index", 0, i);
 }
 
-bool test_cache_iterator(void)
+void test_cache_iterator(void)
 {
-    bool success = true;
-    success &= test_run_test("cache iterator, no gaps", &_test_cache_iterator__cache_no_gaps, NULL, NULL);
-    success &= test_run_test("cache iterator, cache with gaps", &_test_cache_iterator__cache_with_gaps, NULL, NULL);
-    success &= test_run_test("cache iterator, no valid elements, items removed", &_test_cache_iterator__no_valid_items__items_removed, NULL, NULL);
-    return success;
+    struct CacheTestState userstate;
+
+    testing_add_group("cache iterator");
+    testing_add_test("cache iterator, no gaps", &_setup_cache, &_teardown_cache, &_test_cache_iterator__cache_no_gaps, &userstate, sizeof(struct CacheTestState));
+    testing_add_test("cache iterator, cache with gaps", &_setup_cache, &_teardown_cache, &_test_cache_iterator__cache_with_gaps, &userstate, sizeof(struct CacheTestState));
+    testing_add_test("cache iterator, no valid elements, items removed", &_setup_cache, &_teardown_cache, &_test_cache_iterator__no_valid_items__items_removed, &userstate, sizeof(struct CacheTestState));
 }
 
 void test_cache_run_all(void)
 {
-    test_run_test_block("cache iterator", &test_cache_iterator);
+    test_cache_iterator();
 }
