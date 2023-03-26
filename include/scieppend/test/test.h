1fca396a97a4a99adbcf2ff208a7002b2d1ffc2e
Song Taezhou <song.taezhou@gmail.com>
2023-03-26 14:35:15 +0100

    Added missed test.h changes

--- include/scieppend/test/test.h
+++ include/scieppend/test/test.h
@@ -8,7 +8,7 @@
 #define TEST_NAME_MAX 64
 #define TEST_MSG_MAX 256
 
-typedef bool(*test_fn)(void* userstate);
+typedef void(*test_fn)(void* userstate);
 typedef void(*setup_fn)(void* userstate);
 typedef void(*teardown_fn)(void* userstate);
 
@@ -32,6 +32,16 @@ struct Test
     struct TestCase* case_tail;
 };
 
+struct TestGroup
+{
+    struct TestGroup* next;
+    char name[TEST_NAME_MAX];
+    bool success;
+
+    struct Test* test_head;
+    struct Test* test_tail;
+};
+
 bool test_assert_equal_char_buffer(const char* case_name, const char* expect, const char* actual);
 bool test_assert_equal_int(const char* case_name, const int expect, const int actual);
 bool test_assert_equal_bool(const char* case_name, const bool expect, const bool actual);
@@ -42,6 +52,7 @@ bool test_assert_null(const char* case_name, void* value);
 void test_init(void);
 void test_uninit(void);
 
+void testing_add_group(char name[]);
 void testing_add_test(char name[], setup_fn setup, teardown_fn teardown, test_fn test, void* userstate, int userstate_size);
 void testing_run_tests(void);
 void testing_report(void);
