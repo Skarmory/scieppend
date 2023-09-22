61f5fa2da675720c8806f5962fcdbdbaa65fb725
Song Taezhou <song.taezhou@gmail.com>
2023-09-22 14:05:01 +0100

    Added cache map tests to test main

--- src/test/main.c
+++ src/test/main.c
@@ -1,6 +1,7 @@
 #include "scieppend/test/test.h"
 #include "scieppend/test/core/array.h"
 #include "scieppend/test/core/cache.h"
+#include "scieppend/test/core/cache_map.h"
 #include "scieppend/test/core/stack_array.h"
 #include "scieppend/test/core/link_array.h"
 
@@ -12,6 +13,7 @@ int main(int argc, char** argv)
     test_stack_array_run_all();
     test_cache_run_all();
     test_linkarray_run_all();
+    test_cache_map_run_all();
 
     testing_run_tests();
     testing_report();
