9321aa9535917a2241d4cf33e5e1b0384d23f7ce
Song Taezhou <song.taezhou@gmail.com>
2023-09-24 14:55:55 +0100

    Added string_hash(), string_set()
    Untypedefed string

--- include/scieppend/core/string.h
+++ include/scieppend/core/string.h
@@ -6,45 +6,51 @@
 
 #include <stddef.h>
 
-typedef struct _string
+struct string
 {
     char* buffer;
     int   size;
-} string;
+};
 
 /* Create a string with the given characters.
  */
-string* string_new(const char* initial);
+struct string* string_new(const char* initial);
 
 /* Free the given string.
  */
-void string_free(string* str);
+void string_free(struct string* str);
 
 /* Initialise a string.
  * Useful for strings allocated on the stack.
  */
-void string_init(string* str, const char* initial);
+void string_init(struct string* str, const char* initial);
 
 /* Frees the string internals but does not free the string itself.
  */
-void string_uninit(string* str);
+void string_uninit(struct string* str);
 
 /* Get the size of the string in characters used.
  */
-int string_size(string* str);
+int string_size(struct string* str);
 
 /* Wrapper to quickly printf a string.
  */
-void string_printf(string* str);
+void string_printf(struct string* str);
 
 /* Reverse find index from starting point.
  * Iterates backwards over the string from the given start index and returns the start index of the
  * first occurence of a given "needle".
  */
-int string_rfindi(string* str, const char needle, size_t start);
+int string_rfindi(struct string* str, const char needle, size_t start);
+
+void string_set(struct string* str, const char* buffer);
 
 /* Expand the format and set the string.
  */
-void string_format(string* str, const char* format, ...);
+void string_format(struct string* str, const char* format, ...);
+
+/* Hash the string
+ */
+int string_hash(const struct string* str);
 
 #endif
--- src/core/string.c
+++ src/core/string.c
@@ -1,5 +1,7 @@
 #include "scieppend/core/string.h"
 
+#include "scieppend/core/hash.h"
+
 #include <stdarg.h>
 #include <string.h>
 #include <stdio.h>
@@ -7,43 +9,43 @@
 
 // EXTERNAL FUNCS
 
-string* string_new(const char* initial)
+struct string* string_new(const char* initial)
 {
-    string* new_str = malloc(sizeof(string));
+    struct string* new_str = malloc(sizeof(struct string));
     string_init(new_str, initial);
     return new_str;
 }
 
-void string_free(string* str)
+void string_free(struct string* str)
 {
     string_uninit(str);
     free(str);
 }
 
-void string_init(string* str, const char* initial)
+void string_init(struct string* str, const char* initial)
 {
     str->size = strlen(initial);
     str->buffer = malloc(str->size + 1);
     snprintf(str->buffer, str->size + 1, "%s", initial);
 }
 
-void string_uninit(string* str)
+void string_uninit(struct string* str)
 {
     str->size = 0;
     free(str->buffer);
 }
 
-int string_size(string* str)
+int string_size(struct string* str)
 {
     return str->size;
 }
 
-void string_printf(string* str)
+void string_printf(struct string* str)
 {
     printf(str->buffer);
 }
 
-int string_rfindi(string* str, const char needle, size_t start)
+int string_rfindi(struct string* str, const char needle, size_t start)
 {
     const char* haystack = str->buffer;
 
@@ -68,7 +70,19 @@ int string_rfindi(string* str, const char needle, size_t start)
     return -1;
 }
 
-void string_format(string* str, const char* format, ...)
+void string_set(struct string* str, const char* value)
+{
+    int value_len = strlen(value);
+    if(value_len > str->size)
+    {
+        str->buffer = realloc(str->buffer, value_len+1);
+        str->size = value_len;
+    }
+
+    snprintf(str->buffer, str->size, "%s", value);
+}
+
+void string_format(struct string* str, const char* format, ...)
 {
     va_list args;
     va_list args_copy;
@@ -87,4 +101,10 @@ void string_format(string* str, const char* format, ...)
     va_start(args, format);
     vsnprintf(str->buffer, buffer_size, format, args);
     va_end(args);
+}
+
+int string_hash(const struct string* str)
+{
+    int hashed = hash(str->buffer, str->size);
+    return hashed;
 }
